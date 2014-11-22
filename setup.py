d3a3c7979575645e9e3b2afd614a52faa9a4303b
straup <straup@gmail.com>
2014-11-22 16:05:02 +0100

    require -flask 0.35

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup
 
 setup(name='plumbing-shannon-server',
-      version='0.21',
-      description='A Flask based HTTP pony for performing "Shannon-related" calculations on images',
+      version='0.22',
+      description='A Flask-based HTTP pony for performing "Shannon-related" calculations on images',
       author='Cooper Hewitt Smithsonian Design Museum',
       url='https://github.com/cooperhewitt/plumbing-palette-server',
       requires=[
       ],
       dependency_links=[
-          'https://github.com/cooperhewitt/py-cooperhewitt-flask/tarball/master#egg=cooperhewitt.flask-0.34',
+          'https://github.com/cooperhewitt/py-cooperhewitt-flask/tarball/master#egg=cooperhewitt.flask-0.35',
           'https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-shannon/tarball/master#egg=cooperhewitt.roboteyes.shannon-0.2',
       ],
       install_requires=[
@@ -21,5 +21,5 @@ setup(name='plumbing-shannon-server',
       scripts=[
           'scripts/shannon-server.py',
       ],
-      download_url='https://github.com/cooperhewitt/plumbing-shannon-server/tarball/master#egg=plumbing-shannon-server#0.21',
+      download_url='https://github.com/cooperhewitt/plumbing-shannon-server/tarball/master#egg=plumbing-shannon-server#0.22',
       license='BSD')
